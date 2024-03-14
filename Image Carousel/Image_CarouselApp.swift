29a48d58ad120ffd21a6e8666018bd2a296a5a9d
kotathefriends <5070kouta@gmail.com>
2024-03-14 10:41:33 +0900

    Initial Commit

---
+++ Image Carousel/Image_CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_CarouselApp.swift
+//  Image Carousel
+//
+//  Created by KOTA TAKAHASHI on 2024/03/14.
+//
+
+import SwiftUI
+
+@main
+struct Image_CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
